6138b1a9dcec9b0802371d7ad53976ad84fe0ac6
sharkdp <davidpeter@web.de>
2017-08-29 22:02:31 +0200

    Upgrade jquery.terminal

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ pulp browserify --skip-entry-point -m Insect --standalone Insect -O -t insect.js
 cp insect.js web/
 
 cp bower_components/keyboardevent-key-polyfill/index.js web/keyboardevent-key-polyfill.js
-cp bower_components/jquery.terminal/js/jquery.terminal-1.6.2.min.js web/jquery.terminal.min.js
+cp bower_components/jquery.terminal/js/jquery.terminal-1.6.3.min.js web/jquery.terminal.min.js
 cp bower_components/jquery/dist/jquery.min.js web/
 cp bower_components/jquery.terminal/js/jquery.mousewheel-min.js web/
