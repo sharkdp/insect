bff359bffa4ca1817e572646b8430348502b650d
mhmdanas <triallax@tutanota.com>
2022-03-28 21:12:38 +0300

    Remove `fs-extra` dependency in favor of built-in `fs`
    The fewer the dependencies, the better.
    
    Tested with `hyperfine` on my machine (YMMV), the `fs` version is
    actually faster (114.2 ± 3.8 ms with `fs-extra `vs. 76.3 ± 0.8 ms with
    `fs`), but that's not really the point.

--- copy.js
+++ copy.js
@@ -1,14 +1,8 @@
-const { copy } = require("fs-extra");
+const { copyFileSync } = require("fs");
 
-async function copyToWeb() {
-  await Promise.all([
-    copy("insect.js", "web/insect.js"),
-    copy("node_modules/keyboardevent-key-polyfill/index.js", "web/keyboardevent-key-polyfill.js"),
-    copy("node_modules/jquery/dist/jquery.min.js", "web/jquery.min.js"),
-    copy("node_modules/jquery.terminal/js/jquery.terminal.min.js", "web/jquery.terminal.min.js"),
-    copy("node_modules/jquery.terminal/js/jquery.mousewheel-min.js", "web/jquery.mousewheel-min.js"),
-    copy("node_modules/jquery.terminal/css/jquery.terminal.min.css", "web/terminal.css"),
-  ])
-}
-
-copyToWeb()
+copyFileSync("insect.js", "web/insect.js");
+copyFileSync("node_modules/keyboardevent-key-polyfill/index.js", "web/keyboardevent-key-polyfill.js");
+copyFileSync("node_modules/jquery/dist/jquery.min.js", "web/jquery.min.js");
+copyFileSync("node_modules/jquery.terminal/js/jquery.terminal.min.js", "web/jquery.terminal.min.js");
+copyFileSync("node_modules/jquery.terminal/js/jquery.mousewheel-min.js", "web/jquery.mousewheel-min.js");
+copyFileSync("node_modules/jquery.terminal/css/jquery.terminal.min.css", "web/terminal.css");
