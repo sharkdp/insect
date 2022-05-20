94c221c0058a4ac9a8b7f69a03c9efc0922eb12a
mhmdanas <triallax@tutanota.com>
2022-05-20 17:40:46 +0300

    Don't build self-contained Insect binaries anymore
    See https://github.com/sharkdp/insect/issues/297. TL;DR `nexe` currently
    doesn't support building binaries containing recent Node.js versions,
    and we haven't found an alternative yet.
    
    Since the Bash-specific `[[ ]]` syntax is removed in this commit, I also
    set the shebang to use `/bin/sh` instead, removing the dependency on
    Bash.

--- build.sh
+++ build.sh
@@ -1,25 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 npm run browserify
 npm run copy
-
-if [[ $* == *--nexe* ]]
-then
-    echo "Compiling binaries into ./binaries/"
-    # Make a directory to hold the binaries. This should not be checked in to version control.
-    mkdir binaries
-    # Remove the shebang from index.js.
-    tail -n +2 index.js > temp.js
-    # We will target windows, mac, and linux.
-    # Notice that a nexe build target string takes the form
-    # platform-architecture-node_version.
-    # If we want to bump the node version we are using,
-    # we need to change these lines.
-    nexe temp.js -t linux-x64-8.8.1 -o ./binaries/insect-linux-x64
-    nexe temp.js -t linux-x86-8.8.1 -o ./binaries/insect-linux-x86
-    nexe temp.js -t mac-x64-8.8.1 -o ./binaries/insect-mac
-    nexe temp.js -t windows-x64-8.8.1 -o ./binaries/insect-windows-x64.exe
-    nexe temp.js -t windows-x86-8.8.1 -o ./binaries/insect-windows-x86.exe
-    rm temp.js
-    echo "Done compiling binaries."
-fi
