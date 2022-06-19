63776822f1ff242798f555b8ac56a2801cccfcd4
mhmdanas <triallax@tutanota.com>
2022-06-19 15:01:25 +0300

    Use `gfm` instead of deprecated `markdown_github`
    See https://pandoc.org/MANUAL.html#markdown-variants.

--- docs/generate.sh
+++ docs/generate.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-input_format=markdown_github-hard_line_breaks
+
+input_format=gfm-hard_line_breaks
 
 # Generate combined markdown document for README.md
 pandoc --from="$input_format" \
-       --to=markdown_github-raw_html \
+       --to=gfm-raw_html \
        --output=../README.md \
        --standalone \
        --template=template.readme \
